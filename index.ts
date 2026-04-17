123f034040f0c7f41486fa3d6f77a5b611df47b8
Mparween <fariparween17@gmail.com>
2026-04-18 00:41:48 +0530

    Add main App component with routing and title signal

---
+++ index.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Login } from './Pages/login/login';
+import { Home } from './Pages/home/home';
+import { Aboutus } from './Pages/aboutus/aboutus';
+
+@Component({
+  selector: 'app-root1',
+  imports: [CommonModule, RouterLink, RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('demo');
+}
